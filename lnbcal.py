14c4142779dcc42269410b7fd8ed3818776db297
Vaishu03 <86002684+Vaishu03@users.noreply.github.com>
2022-04-15 18:54:09 +0530

    Add files via upload

---
+++ lnbcal.py
@@ -0,0 +1,8 @@
+num1 = int(input())
+num2 = int(input())
+product = num1*num2
+print(product)
+if product > 500:
+    print(num1+num2)
+elif product < 500:
+    print("Hello LNB code is running fine !!")
